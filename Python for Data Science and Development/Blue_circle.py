2ef1eadcd16fba6cdf861e6a5ec8f78cfd1e8a01
Novobura <novobura@gmail.com>
2023-09-02 10:56:38 +0900

    Add files via upload

---
+++ Python for Data Science and Development/Blue_circle.py
@@ -0,0 +1,13 @@
+#Objects practice 
+
+BlueCircle = Circle(radius=100)
+
+dir(BlueCircleCircle)
+
+print('Radius of object:',BlueCircleCircle.radius)
+BlueCircleCircle.add_radius(2)
+print('Radius of object of after applying the method add_radius(2):',BlueCircle.radius)
+BlueCircle.add_radius(5)
+print('Radius of object of after applying the method add_radius(5):',BlueCircle.radius)
+
+BlueCircle.drawCircle()
